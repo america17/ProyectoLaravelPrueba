e13f034b654556e25a850f898f40244caea4e682
america17 <20170067@uttcampus.edu.mx>
2023-03-16 15:50:42 -0600

    correo enviado

--- resources/views/vistasemail/correoEnviado.blade.php
+++ resources/views/vistasemail/correoEnviado.blade.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    
+
     <title>Correo enviado</title>
+
+    <style>
+        body {
+            background-color: rgb(2, 0, 36);
+            background-color: linear-gradient(0deg, rgba(2, 0, 36, 1) 0%, rgba(118, 117, 178, 1) 32%, rgba(141, 140, 231, 1) 42%, rgba(181, 181, 255, 1) 54%, rgba(165, 165, 255, 1) 89%);
+        }
+
+        #txt {
+            color:gainsboro;
+        }
+    </style>
 </head>
-<body>
-    <div class="d-flex justify-content-center">
-        <h1>El correo ha salido con éxito</h1> <br>
-        <h3>Revisa tu bandeja para poder acceder</h3>
+
+<body style="background-color: darkslateblue;">
+    <div>
+        <div id="txt" style="position: absolute; top: 10%; left: 50%; transform: translate(-50%, -50%);">
+            <h1>El correo se ha enviado con éxito</h1>
+        </div>
+        <div id="txt" style="position: absolute; top: 18%; left: 50%; transform: translate(-50%, -50%);">
+            <h3>Revisa tu bandeja para poder acceder</h3>
+        </div>
+        <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <img src="https://images.vexels.com/media/users/3/136340/isolated/preview/74ac661d8216442ae469efb39c9584dc-icono-de-mensaje-de-correo.png" alt="">
+        </div>
+        <div id="txt" style="position: absolute; top: 85%; left: 50%; transform: translate(-50%, -50%);">
+            <h5>Puedes cerrar esta ventana</h5>
+        </div>
+
+        <img src="/resources/views/img/Correo.png" alt="">
     </div>
 </body>
+
 </html>
